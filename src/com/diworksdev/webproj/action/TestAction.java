95771469d9ef18d23b5a245559d9683523d3749c
uchikosi yuuki <ucchikohiwithsugar@gmail.com>
2024-01-10 22:24:13 +0900

    コメント追加

--- src/com/diworksdev/webproj/action/TestAction.java
+++ src/com/diworksdev/webproj/action/TestAction.java
@@ -2,10 +2,14 @@ package com.diworksdev.webproj.action;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class TestAction extends ActionSupport{
+//	TestAction は ActionSupport クラスを継承
+//	ActionSupport は Struts 2 アクションの基本的な機能を提供する抽象クラス
 	private String username;
 	private String password;
+//	username と password は、このアクションクラスのフィールド（属性）です。これらはHTMLフォームからの入力データを保持するための変数です。
 	public String execute(){
 		return SUCCESS;
+//		execute メソッドが SUCCESS を返しています。SUCCESS は Struts 2 が提供する定数で、アクションが成功したことを示します。
 	}
 	public String getUsername() {
 		return username;
